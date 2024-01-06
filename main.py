f98776e4c453de003d5bd54a3f23f1a727f70796
Sahil Rana <sahilrana1773@gmail.com>
2024-01-06 18:04:37 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+original = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(original)
+encr = chars.copy()
+random.shuffle(encr)
+print(f"chars: {original}")
+print(f"key:{encr}")
+
+#encryption
+encrypt = input("Enter a message to encrypt: ")
+encrypted =" "
+
+for letter in encrypt:
+    index = original.index(letter)
+    encrypted += encr[index]
+
+print(f"original message {encrypt}")
+print(f"encrypted message {encrypted}")
+
+#decryption
+encrypted = input("Enter a message to decrypt: ")
+encrypt =" "
+
+for letter in encrypted:
+    index = encr.index(letter)
+    encrypt += original[index]
+
+print(f"original message {encrypted}")
+print(f"encrypted message {encrypt}")
+
+
+
+
